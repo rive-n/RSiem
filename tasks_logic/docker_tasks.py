b7006af707524d382c257d6b0b62a8c789326d99
Saschenko Danila <d.saschenko@NB0304.local>
2022-04-19 19:49:15 +0300

    Images build logic fix, docker run logic fix

--- tasks_logic/docker_tasks.py
+++ tasks_logic/docker_tasks.py
@@ -5,7 +5,7 @@ from dramatiq.brokers.rabbitmq import RabbitmqBroker
 from dramatiq.results.backends.redis import RedisBackend
 from dramatiq.results import Results
 
-from os import environ
+from os import environ, chdir
 
 import pika
 
@@ -17,12 +17,12 @@ if environ.get("DEBUG", True) == 'True':
     redis_backend = RedisBackend(host="127.0.0.1")
 else:
     # docker network env #
-    rabbitmq_broker = RabbitmqBroker(host="rabbitmq",  credentials=creds)
+    rabbitmq_broker = RabbitmqBroker(host="rabbitmq", credentials=creds)
     redis_backend = RedisBackend(host="redis")
-
 
 dramatiq.set_broker(rabbitmq_broker)
 rabbitmq_broker.add_middleware(Results(backend=redis_backend))
+
 
 @dramatiq.actor
 def print_result(message_data, result):
@@ -37,7 +37,8 @@ def print_error(message_data, exception_data):
 
 
 @dramatiq.actor(store_results=True)
-def build_docker_instance(*args, **kwargs): #args - dramatiq bypass
+def build_docker_instance(*args, **kwargs):  # args - dramatiq bypass
+    chdir(kwargs['full_path'])
     build_command = ["docker", "build", ".", "-f",
                      f"{kwargs['full_path'] + '/' if kwargs['full_path'][-1:] != '/' else kwargs['full_path'] + ''}{kwargs['dockerfile']}",
                      "-t", kwargs['service_name']]
@@ -51,8 +52,10 @@ def build_docker_instance(*args, **kwargs): #args - dramatiq bypass
 
 
 @dramatiq.actor(store_results=True)
-def run_docker_instance(*args, **kwargs): #args - dramatiq bypass
-    run_command = ['docker', 'run', '-p', f"{kwargs['service_port']}:{kwargs['service_port']}", '-t', kwargs['service_name']]
+def run_docker_instance(*args, **kwargs):  # args - dramatiq bypass
+    chdir(kwargs['full_path'])
+    run_command = ['docker', 'run', '--rm', '-dp', f"{kwargs['service_port']}:{kwargs['service_port']}",
+                   "--name", kwargs['service_name'], '--network', 'diplomsiem_default', '-t', kwargs['service_name']]
     output = ""
     try:
         output += subprocess.check_output(run_command, stderr=subprocess.STDOUT).decode()
@@ -62,6 +65,7 @@ def run_docker_instance(*args, **kwargs): #args - dramatiq bypass
 
 
 if __name__ == '__main__':
-    print(build_docker_instance(full_path="/Users/d.saschenko/PycharmProjects/diplomSiem/internal/Services/anotherService",
-                          dockerfile="Dockerfile", service_name="testname"))
-    print(run_docker_instance(service_name="testname", port="5432"))
+    print(build_docker_instance(full_path="/Users/d.saschenko/PycharmProjects/diplomSiem/internal/Services/testRiven",
+                                dockerfile="Dockerfile", service_name="flaskapp"))
+    print(run_docker_instance(service_name="flaskapp", service_port="8888",
+                              full_path="/Users/d.saschenko/PycharmProjects/diplomSiem/internal/Services/testRiven"))
